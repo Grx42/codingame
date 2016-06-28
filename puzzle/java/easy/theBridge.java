4fe5b75fd2cb71cb78f6e21de2fe137eeba7b993
simplon-gregoryg <grexor@hotmail.com>
2016-06-28 16:53:16 +0200

    Improve theBridge.java

--- puzzle/java/easy/theBridge.java
+++ puzzle/java/easy/theBridge.java
@@ -25,11 +25,7 @@ class Player {
             // To debug: System.err.println("Debug messages...");
 
             // A single line containing one of 4 keywords: SPEED, SLOW, JUMP, WAIT.
-            if (speed > (gap +1))
-            {
-                System.out.println("SLOW");
-            }
-            else if (jumped && speed > 0)
+            if (speed > (gap +1) || (jumped && speed > 0))
             {
                 System.out.println("SLOW");
             }
